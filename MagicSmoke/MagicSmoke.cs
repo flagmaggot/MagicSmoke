f6e837990f1764b15194076936dbb86075533e14
s <shawn.royal@gmail.com>
2018-08-23 15:55:52 -0400

    added dual wield

--- MagicSmoke/MagicSmoke.cs
+++ MagicSmoke/MagicSmoke.cs
@@ -1,38 +1,127 @@
 ﻿using System;
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace MagicSmoke
 {
     public class MagicSmoke : ETGModule
     {
-        public override void Exit()
+        private ConsoleCommandGroup _MsGroup;
+        private ConsoleCommandGroup _SetGroup;
+        private ConsoleCommandGroup _GetGroup;
+        protected static AutocompletionSettings _AutocompletionSettings;
+
+        private Dictionary<string, PlayerStats.StatType> _Stats = new Dictionary<string, PlayerStats.StatType>
         {
-            throw new NotImplementedException();
+            ["movespeed"] = PlayerStats.StatType.MovementSpeed,
+            ["firerate"] = PlayerStats.StatType.RateOfFire,
+            ["spread"] = PlayerStats.StatType.Accuracy,
+            ["health"] = PlayerStats.StatType.Health,
+            ["coolness"] = PlayerStats.StatType.Coolness,
+            ["damage"] = PlayerStats.StatType.Damage,
+            ["projspeed"] = PlayerStats.StatType.ProjectileSpeed,
+            ["guncapacity"] = PlayerStats.StatType.AdditionalGunCapacity,
+            ["itemcapacity"] = PlayerStats.StatType.AdditionalItemCapacity,
+            ["ammomult"] = PlayerStats.StatType.AmmoCapacityMultiplier,
+            ["reloadspeed"] = PlayerStats.StatType.ReloadSpeed,
+            ["shotpiercing"] = PlayerStats.StatType.AdditionalShotPiercing,
+            ["knockbackmult"] = PlayerStats.StatType.KnockbackMultiplier,
+            ["globalpricemult"] = PlayerStats.StatType.GlobalPriceMultiplier,
+            ["curse"] = PlayerStats.StatType.Curse,
+            ["bulletscale"] = PlayerStats.StatType.PlayerBulletScale,
+            ["clipmult"] = PlayerStats.StatType.AdditionalClipCapacityMultiplier,
+            ["shotbounces"] = PlayerStats.StatType.AdditionalShotBounces,
+            ["blanksperfloor"] = PlayerStats.StatType.AdditionalBlanksPerFloor,
+            ["shadowbulletchance"] = PlayerStats.StatType.ShadowBulletChance,
+            ["throwngundamage"] = PlayerStats.StatType.ThrownGunDamage,
+            ["dodgedamage"] = PlayerStats.StatType.DodgeRollDamage,
+            ["damagetobosses"] = PlayerStats.StatType.DamageToBosses,
+            ["enemyprojspeed"] = PlayerStats.StatType.EnemyProjectileSpeedMultiplier,
+            ["extremeshadowchance"] = PlayerStats.StatType.ExtremeShadowBulletChance,
+            ["chargemult"] = PlayerStats.StatType.ChargeAmountMultiplier,
+            ["rangemult"] = PlayerStats.StatType.RangeMultiplier,
+            ["dodgedistance"] = PlayerStats.StatType.DodgeRollDistanceMultiplier,
+            ["dodgespeed"] = PlayerStats.StatType.DodgeRollSpeedMultiplier,
+            ["tarnishclipmult"] = PlayerStats.StatType.TarnisherClipCapacityMultiplier,
+            ["moneymult"] = PlayerStats.StatType.MoneyMultiplierFromEnemies
+        };
+
+        private static Action<string[]> _StatSet;
+        private static Action<string[]> _StatGet;
+        private static Action<string[]> _StatGetAll;
+
+
+        public void SetStat(PlayerController player, PlayerStats.StatType type, float val)
+        {
+            GameManager.Instance.PrimaryPlayer.stats.SetBaseStatValue(type, val, player);
         }
 
-        public static int speed;
-        public static bool speedChanged;
-
-        static MagicSmoke()
+        public float GetStat(PlayerController player, PlayerStats.StatType type)
         {
-            MagicSmoke.speed = 7;
-            speedChanged = false;
+            //return stats.GetStatValue(type);
+            return GameManager.Instance.PrimaryPlayer.stats.GetStatValue(type);
         }
 
-        public MagicSmoke()
-        { }
+        public void AddStatCommands(PlayerController player, string name, PlayerStats.StatType type)
+        {
+            _SetGroup.AddUnit(name, (args) => SetStat(player, type, float.Parse(args[0])));
+            _GetGroup.AddUnit(name, (args) => ETGModConsole.Log(GetStat(player, type).ToString()));
+        }
 
-        
         public override void Init()
         {
-            ETGMod.Objects.AddHook<PlayerStats>(new ComponentHook(this.NewPlayerStats));
-            ETGModConsole.Commands.AddGroup("ms", (string[] args) => {
+            _AutocompletionSettings = new AutocompletionSettings((input) => {
+                List<string> ret = new List<string>();
+                foreach (var stat in _Stats)
+                {
+                    if (stat.Key.AutocompletionMatch(input.ToLower()))
+                    {
+                        ret.Add(stat.Key);
+                    }
+                }
+                return ret.ToArray();
+            });
+
+            _StatGet = (args) => {
+                if (_Stats.ContainsKey(args[0]))
+                {
+                    ETGModConsole.Log(_Stats[args[0]] + " value is: <color=#ff0000ff>" + GameManager.Instance.PrimaryPlayer.stats.GetStatValue(_Stats[args[0]]) + "</color>");
+                }
+                else
+                    ETGModConsole.Log($"Please check your command and try again");
+            };
+
+            _StatSet = (args) => {
+                
+                float value;
+                if (_Stats.ContainsKey(args[0]) && float.TryParse(args[1], out value))
+                {
+                    GameManager.Instance.PrimaryPlayer.stats.SetBaseStatValue(_Stats[args[0]], value, GameManager.Instance.PrimaryPlayer);
+                    ETGModConsole.Log(_Stats[args[0]] + " set to: <color=#ff0000ff>" + value + "</color>");
+                }
+                else
+                    ETGModConsole.Log($"Please check your command and try again");
+            };
+
+            _StatGetAll = (args) => {
+                ETGModConsole.Log("Available stats:");
+                foreach (var stat in _Stats)
+                {
+                    ETGModConsole.Log($"- {stat.Key} <color=#ff0000ff>" + GameManager.Instance.PrimaryPlayer.stats.GetStatValue(stat.Value) + "</color>");
+                }
+            };
+
+            ETGModConsole.Commands.AddGroup("ms", (string[] args) =>
+            {
                 ETGModConsole.Log("<size=100><color=#ff0000ff>MagicSmoke v1 by flagmaggot</color></size>", false);
                 ETGModConsole.Log("<size=100><color=#ff0000ff>DOES NOT WORK WHILE IN THE BREACH</color></size>", false);
                 ETGModConsole.Log("<size=100><color=#ff0000ff>--------------------------------</color></size>", false);
                 ETGModConsole.Log("Use \"ms help\" for help!", false);
+
             });
-            //ETGModConsole.Commands.AddGroup("ms");
+            
+
+
             ETGModConsole.Commands.GetGroup("ms").AddUnit("help", (string[] args) =>
             {
                 ETGModConsole.Log("<size=100><color=#ff0000ff>MagicSmoke v1 by flagmaggot</color></size>", false);
@@ -41,48 +130,40 @@ namespace MagicSmoke
                 ETGModConsole.Log("Magic Smoke Command Reference:", false);
                 ETGModConsole.Log("", false);
                 ETGModConsole.Log("ms help - Displays this help", false);
-                ETGModConsole.Log("ms get_player_speed - gets the player speed", false);
-                ETGModConsole.Log("ms set_player_speed [arg] - sets the player speed (integers only)", false);
+                ETGModConsole.Log("ms get <stat name> - gets the state value", false);
+                ETGModConsole.Log("ms set <stat name> [arg] - sets the player speed (decimal values)", false);
+                ETGModConsole.Log("ms getallstats - returns all the stats and their values", false);
             });
-            ETGModConsole.Commands.GetGroup("ms").AddUnit("set_player_speed", new Action<string[]>(this.setSpeedCommand));
-            ETGModConsole.Commands.GetGroup("ms").AddUnit("get_player_speed", new Action<string[]>(this.getSpeedCommand));
+
+            ETGModConsole.Commands.GetGroup("ms").AddUnit("forcedualwield", (string[] args) =>
+            {
+                if (args.Length < 1) throw new Exception("At least 1 argument required.");
+                var partner_id = int.Parse(args[0]);
+                var player = GameManager.Instance.PrimaryPlayer;
+                var gun = player.inventory.CurrentGun;
+                var partner_gun = PickupObjectDatabase.GetById(partner_id) as Gun;
+                player.inventory.AddGunToInventory(partner_gun);
+                var forcer = gun.gameObject.AddComponent<DualWieldForcer>();
+                forcer.Gun = gun;
+                forcer.PartnerGunID = partner_gun.PickupObjectId;
+                forcer.TargetPlayer = player;
+                
+            });
+
+            _MsGroup = ETGModConsole.Commands.GetGroup("ms");
+            _SetGroup = _MsGroup.AddUnit("set", _StatSet, _AutocompletionSettings);
+            _SetGroup = _MsGroup.AddUnit("get", _StatGet, _AutocompletionSettings);
+            _GetGroup = _MsGroup.AddUnit("getallstats", _StatGetAll, _AutocompletionSettings);
         }
 
         public override void Start()
         {
-            
+            throw new NotImplementedException();
         }
 
-        //private PlayerStats m_playerStats;
-        private void setSpeedCommand(string[] args)
+        public override void Exit()
         {
-            if (args.Length != 0)
-            {
-                int value;
-                if (int.TryParse(args[0], out value))
-                {
-                    ETGModConsole.Log(string.Concat("Player speed set to: <color=#ff0000ff>", value, "</color>"));
-                    speed = value;
-                    speedChanged = true;
-                }
-                else
-                    ETGModConsole.Log(string.Concat(args[0], " is not a valid number, try again"));
-
-            }
+            throw new NotImplementedException();
         }
-
-        private void getSpeedCommand(string[] args)
-        {
-            ETGModConsole.Log(string.Concat("Player speed is: <color=#ff0000ff>", stats.MovementSpeed, "</color>"));
-        }
-
-        private PlayerStats stats;
-        private void NewPlayerStats(Component component)
-        {
-            stats = component as PlayerStats;
-            if(stats.GetComponent<SpeedBehavior>() == null)
-                stats.gameObject.AddComponent<SpeedBehavior>();
-        }
-
     }
 }
---
+++ MagicSmoke/StatsBehavior.cs
@@ -0,0 +1,186 @@
+﻿//using UnityEngine;
+
+//#pragma warning disable 0246
+
+//namespace MagicSmoke
+//{
+//    class StatsBehavior : MonoBehaviour
+//    {
+//        private PlayerStats m_playerStats;
+//        private PlayerController m_playerController;
+
+//        public StatsBehavior()
+//        {
+//        }
+
+//        public void Start()
+//        {
+//        }
+
+//        public void updateStat(bool nameStatIsChanged, PlayerStats.StatType stat, float statValue)
+//        {
+//            if (nameStatIsChanged)
+//            {
+//                m_playerStats = base.GetComponent<PlayerStats>();
+//                m_playerController = base.GetComponent<PlayerController>();
+//                m_playerStats.SetBaseStatValue(stat, (float)statValue, m_playerController);
+//                nameStatIsChanged = false;
+//            }
+//        }
+
+
+//        public void Update()
+//        {
+//            updateStat(MagicSmoke.MovementSpeedChanged, PlayerStats.StatType.MovementSpeed, (float)MagicSmoke.MovementSpeed);
+//            updateStat(MagicSmoke.RateOfFireChanged, PlayerStats.StatType.RateOfFire, (float)MagicSmoke.RateOfFire);
+//            updateStat(MagicSmoke.AccuracyChanged, PlayerStats.StatType.Accuracy, (float)MagicSmoke.Accuracy);
+//            updateStat(MagicSmoke.HealthChanged, PlayerStats.StatType.Health, (float)MagicSmoke.Health);
+//            updateStat(MagicSmoke.CoolnessChanged, PlayerStats.StatType.Coolness, (float)MagicSmoke.Coolness);
+//            updateStat(MagicSmoke.DamageChanged, PlayerStats.StatType.Damage, (float)MagicSmoke.Damage);
+//            updateStat(MagicSmoke.ProjectileSpeedChanged, PlayerStats.StatType.ProjectileSpeed, (float)MagicSmoke.ProjectileSpeed);
+//            updateStat(MagicSmoke.AdditionalGunCapacityChanged, PlayerStats.StatType.AdditionalGunCapacity, (float)MagicSmoke.AdditionalGunCapacity);
+//            updateStat(MagicSmoke.AdditionalItemCapacityChanged, PlayerStats.StatType.AdditionalItemCapacity, (float)MagicSmoke.AdditionalItemCapacity);
+//            updateStat(MagicSmoke.AmmoCapacityMultiplierChanged, PlayerStats.StatType.AmmoCapacityMultiplier, (float)MagicSmoke.AmmoCapacityMultiplier);
+//            updateStat(MagicSmoke.AmmoCapacityMultiplierChanged, PlayerStats.StatType.AmmoCapacityMultiplier, (float)MagicSmoke.AmmoCapacityMultiplier);
+//            updateStat(MagicSmoke.ReloadSpeedChanged, PlayerStats.StatType.ReloadSpeed, (float)MagicSmoke.ReloadSpeed);
+//            updateStat(MagicSmoke.AdditionalShotPiercingChanged, PlayerStats.StatType.AdditionalShotPiercing, (float)MagicSmoke.AdditionalShotPiercing);
+//            updateStat(MagicSmoke.KnockbackMultiplierChanged, PlayerStats.StatType.KnockbackMultiplier, (float)MagicSmoke.KnockbackMultiplier);
+//            updateStat(MagicSmoke.GlobalPriceMultiplierChanged, PlayerStats.StatType.GlobalPriceMultiplier, (float)MagicSmoke.GlobalPriceMultiplier);
+//            updateStat(MagicSmoke.CurseChanged, PlayerStats.StatType.Curse, (float)MagicSmoke.Curse);
+//            updateStat(MagicSmoke.PlayerBulletScaleChanged, PlayerStats.StatType.PlayerBulletScale, (float)MagicSmoke.PlayerBulletScale);
+//            updateStat(MagicSmoke.MovementSpeedChanged, PlayerStats.StatType.MovementSpeed, (float)MagicSmoke.MovementSpeed);
+//            updateStat(MagicSmoke.MovementSpeedChanged, PlayerStats.StatType.MovementSpeed, (float)MagicSmoke.MovementSpeed);
+//            updateStat(MagicSmoke.MovementSpeedChanged, PlayerStats.StatType.MovementSpeed, (float)MagicSmoke.MovementSpeed);
+//            updateStat(MagicSmoke.MovementSpeedChanged, PlayerStats.StatType.MovementSpeed, (float)MagicSmoke.MovementSpeed);
+
+
+
+
+//            //if (MagicSmoke.AdditionalClipCapacityMultiplierChanged)
+//            //{
+//            //    m_playerStats = base.GetComponent<PlayerStats>();
+//            //    m_playerController = base.GetComponent<PlayerController>();
+//            //    m_playerStats.SetBaseStatValue(PlayerStats.StatType.AdditionalClipCapacityMultiplier, (float)MagicSmoke.AdditionalClipCapacityMultiplier, m_playerController);
+//            //    MagicSmoke.AdditionalClipCapacityMultiplierChanged = false;
+//            //}
+
+//            //if (MagicSmoke.AdditionalShotBouncesChanged)
+//            //{
+//            //    m_playerStats = base.GetComponent<PlayerStats>();
+//            //    m_playerController = base.GetComponent<PlayerController>();
+//            //    m_playerStats.SetBaseStatValue(PlayerStats.StatType.AdditionalShotBounces, (float)MagicSmoke.AdditionalShotBounces, m_playerController);
+//            //    MagicSmoke.AdditionalShotBouncesChanged = false;
+//            //}
+
+//            //if (MagicSmoke.AdditionalBlanksPerFloorChanged)
+//            //{
+//            //    m_playerStats = base.GetComponent<PlayerStats>();
+//            //    m_playerController = base.GetComponent<PlayerController>();
+//            //    m_playerStats.SetBaseStatValue(PlayerStats.StatType.AdditionalBlanksPerFloor, (float)MagicSmoke.AdditionalBlanksPerFloor, m_playerController);
+//            //    MagicSmoke.AdditionalBlanksPerFloorChanged = false;
+//            //}
+
+//            //if (MagicSmoke.ShadowBulletChanceChanged)
+//            //{
+//            //    m_playerStats = base.GetComponent<PlayerStats>();
+//            //    m_playerController = base.GetComponent<PlayerController>();
+//            //    m_playerStats.SetBaseStatValue(PlayerStats.StatType.ShadowBulletChance, (float)MagicSmoke.ShadowBulletChance, m_playerController);
+//            //    MagicSmoke.ShadowBulletChanceChanged = false;
+//            //}
+
+//            //if (MagicSmoke.ThrownGunDamageChanged)
+//            //{
+//            //    m_playerStats = base.GetComponent<PlayerStats>();
+//            //    m_playerController = base.GetComponent<PlayerController>();
+//            //    m_playerStats.SetBaseStatValue(PlayerStats.StatType.ThrownGunDamage, (float)MagicSmoke.ThrownGunDamage, m_playerController);
+//            //    MagicSmoke.ThrownGunDamageChanged = false;
+//            //}
+
+//            //if (MagicSmoke.DodgeRollDamageChanged)
+//            //{
+//            //    m_playerStats = base.GetComponent<PlayerStats>();
+//            //    m_playerController = base.GetComponent<PlayerController>();
+//            //    m_playerStats.SetBaseStatValue(PlayerStats.StatType.DodgeRollDamage, (float)MagicSmoke.DodgeRollDamage, m_playerController);
+//            //    MagicSmoke.DodgeRollDamageChanged = false;
+//            //}
+
+//            //if (MagicSmoke.DamageToBossesChanged)
+//            //{
+//            //    m_playerStats = base.GetComponent<PlayerStats>();
+//            //    m_playerController = base.GetComponent<PlayerController>();
+//            //    m_playerStats.SetBaseStatValue(PlayerStats.StatType.DamageToBosses, (float)MagicSmoke.DamageToBosses, m_playerController);
+//            //    MagicSmoke.DamageToBossesChanged = false;
+//            //}
+
+//            //if (MagicSmoke.EnemyProjectileSpeedMultiplierChanged)
+//            //{
+//            //    m_playerStats = base.GetComponent<PlayerStats>();
+//            //    m_playerController = base.GetComponent<PlayerController>();
+//            //    m_playerStats.SetBaseStatValue(PlayerStats.StatType.EnemyProjectileSpeedMultiplier, (float)MagicSmoke.EnemyProjectileSpeedMultiplier, m_playerController);
+//            //    MagicSmoke.EnemyProjectileSpeedMultiplierChanged = false;
+//            //}
+
+//            //if (MagicSmoke.ExtremeShadowBulletChanceChanged)
+//            //{
+//            //    m_playerStats = base.GetComponent<PlayerStats>();
+//            //    m_playerController = base.GetComponent<PlayerController>();
+//            //    m_playerStats.SetBaseStatValue(PlayerStats.StatType.ExtremeShadowBulletChance, (float)MagicSmoke.ExtremeShadowBulletChance, m_playerController);
+//            //    MagicSmoke.ExtremeShadowBulletChanceChanged = false;
+//            //}
+
+//            //if (MagicSmoke.ChargeAmountMultiplierChanged)
+//            //{
+//            //    m_playerStats = base.GetComponent<PlayerStats>();
+//            //    m_playerController = base.GetComponent<PlayerController>();
+//            //    m_playerStats.SetBaseStatValue(PlayerStats.StatType.ChargeAmountMultiplier, (float)MagicSmoke.ChargeAmountMultiplier, m_playerController);
+//            //    MagicSmoke.ChargeAmountMultiplierChanged = false;
+//            //}
+
+//            //if (MagicSmoke.RangeMultiplierChanged)
+//            //{
+//            //    m_playerStats = base.GetComponent<PlayerStats>();
+//            //    m_playerController = base.GetComponent<PlayerController>();
+//            //    m_playerStats.SetBaseStatValue(PlayerStats.StatType.RangeMultiplier, (float)MagicSmoke.RangeMultiplier, m_playerController);
+//            //    MagicSmoke.RangeMultiplierChanged = false;
+//            //}
+
+//            //if (MagicSmoke.DodgeRollDistanceMultiplierChanged)
+//            //{
+//            //    m_playerStats = base.GetComponent<PlayerStats>();
+//            //    m_playerController = base.GetComponent<PlayerController>();
+//            //    m_playerStats.SetBaseStatValue(PlayerStats.StatType.DodgeRollDistanceMultiplier, (float)MagicSmoke.DodgeRollDistanceMultiplier, m_playerController);
+//            //    MagicSmoke.DodgeRollDistanceMultiplierChanged = false;
+//            //}
+
+//            //if (MagicSmoke.DodgeRollSpeedMultiplierChanged)
+//            //{
+//            //    m_playerStats = base.GetComponent<PlayerStats>();
+//            //    m_playerController = base.GetComponent<PlayerController>();
+
+//            //    m_playerStats.SetBaseStatValue(PlayerStats.StatType.DodgeRollSpeedMultiplier, (float)MagicSmoke.DodgeRollSpeedMultiplier, m_playerController);
+//            //    MagicSmoke.DodgeRollSpeedMultiplierChanged = false;
+//            //}
+
+//            //if (MagicSmoke.TarnisherClipCapacityMultiplierChanged)
+//            //{
+//            //    m_playerStats = base.GetComponent<PlayerStats>();
+//            //    m_playerController = base.GetComponent<PlayerController>();
+//            //    m_playerStats.SetBaseStatValue(PlayerStats.StatType.TarnisherClipCapacityMultiplier, (float)MagicSmoke.TarnisherClipCapacityMultiplier, m_playerController);
+//            //    MagicSmoke.TarnisherClipCapacityMultiplierChanged = false;
+//            //}
+
+//            //if (MagicSmoke.MoneyMultiplierFromEnemiesChanged)
+//            //{
+//            //    m_playerStats = base.GetComponent<PlayerStats>();
+//            //    m_playerController = base.GetComponent<PlayerController>();
+//            //    m_playerStats.SetBaseStatValue(PlayerStats.StatType.MoneyMultiplierFromEnemies, (float)MagicSmoke.MoneyMultiplierFromEnemies, m_playerController);
+//            //    MagicSmoke.MoneyMultiplierFromEnemiesChanged = false;
+//            //}
+
+
+//        }
+
+        
+
+//    }
+//}
