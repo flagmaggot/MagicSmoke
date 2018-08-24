503cb6d4f9161cb113ee6b1d77773efdb93601de
s <shawn.royal@gmail.com>
2018-08-24 07:55:59 -0400

    added synergy and glitch chest spawn commands

--- MagicSmoke/MagicSmoke.cs
+++ MagicSmoke/MagicSmoke.cs
@@ -9,7 +9,9 @@ namespace MagicSmoke
         private ConsoleCommandGroup _MsGroup;
         private ConsoleCommandGroup _SetGroup;
         private ConsoleCommandGroup _GetGroup;
+        private ConsoleCommandGroup _SynergyChestGroup;
         protected static AutocompletionSettings _AutocompletionSettings;
+        protected static AutocompletionSettings _AutocompletionChests;
 
         private Dictionary<string, PlayerStats.StatType> _Stats = new Dictionary<string, PlayerStats.StatType>
         {
@@ -46,10 +48,24 @@ namespace MagicSmoke
             ["moneymult"] = PlayerStats.StatType.MoneyMultiplierFromEnemies
         };
 
+        private Dictionary<string, Chest> _Chests = new Dictionary<string, Chest>
+        {
+            ["a"] = GameManager.Instance.RewardManager.A_Chest,
+            ["b"] = GameManager.Instance.RewardManager.B_Chest,
+            ["c"] = GameManager.Instance.RewardManager.C_Chest,
+            ["d"] = GameManager.Instance.RewardManager.D_Chest,
+            ["s"] = GameManager.Instance.RewardManager.S_Chest,
+            ["red"] = GameManager.Instance.RewardManager.A_Chest,
+            ["green"] = GameManager.Instance.RewardManager.B_Chest,
+            ["blue"] = GameManager.Instance.RewardManager.C_Chest,
+            ["brown"] = GameManager.Instance.RewardManager.D_Chest,
+            ["black"] = GameManager.Instance.RewardManager.S_Chest
+        };
+
         private static Action<string[]> _StatSet;
         private static Action<string[]> _StatGet;
         private static Action<string[]> _StatGetAll;
-
+        private static Action<string[]> _GlitchSpawn;
 
         public void SetStat(PlayerController player, PlayerStats.StatType type, float val)
         {
@@ -58,7 +74,6 @@ namespace MagicSmoke
 
         public float GetStat(PlayerController player, PlayerStats.StatType type)
         {
-            //return stats.GetStatValue(type);
             return GameManager.Instance.PrimaryPlayer.stats.GetStatValue(type);
         }
 
@@ -82,6 +97,18 @@ namespace MagicSmoke
                 return ret.ToArray();
             });
 
+            _AutocompletionChests = new AutocompletionSettings((input) => {
+                List<string> ret = new List<string>();
+                foreach (var stat in _Chests)
+                {
+                    if (stat.Key.AutocompletionMatch(input.ToLower()))
+                    {
+                        ret.Add(stat.Key);
+                    }
+                }
+                return ret.ToArray();
+            });
+
             _StatGet = (args) => {
                 if (_Stats.ContainsKey(args[0]))
                 {
@@ -91,13 +118,14 @@ namespace MagicSmoke
                     ETGModConsole.Log($"Please check your command and try again");
             };
 
-            _StatSet = (args) => {
-                
-                float value;
-                if (_Stats.ContainsKey(args[0]) && float.TryParse(args[1], out value))
+            _GlitchSpawn = (args) => {
+                if (_Chests.ContainsKey(args[0]))
                 {
-                    GameManager.Instance.PrimaryPlayer.stats.SetBaseStatValue(_Stats[args[0]], value, GameManager.Instance.PrimaryPlayer);
-                    ETGModConsole.Log(_Stats[args[0]] + " set to: <color=#ff0000ff>" + value + "</color>");
+                    Chest glitchedchest = _Chests[args[0]];
+                    glitchedchest.ForceGlitchChest = true;
+                    glitchedchest.BecomeGlitchChest();
+                    IntVector2 basePosition = new IntVector2((int)GameManager.Instance.PrimaryPlayer.transform.position.x, (int)GameManager.Instance.PrimaryPlayer.transform.position.y);
+                    Chest.Spawn(glitchedchest, basePosition);
                 }
                 else
                     ETGModConsole.Log($"Please check your command and try again");
@@ -121,7 +149,6 @@ namespace MagicSmoke
             });
             
 
-
             ETGModConsole.Commands.GetGroup("ms").AddUnit("help", (string[] args) =>
             {
                 ETGModConsole.Log("<size=100><color=#ff0000ff>MagicSmoke v1 by flagmaggot</color></size>", false);
@@ -137,22 +164,46 @@ namespace MagicSmoke
 
             ETGModConsole.Commands.GetGroup("ms").AddUnit("forcedualwield", (string[] args) =>
             {
-                if (args.Length < 1) throw new Exception("At least 1 argument required.");
-                var partner_id = int.Parse(args[0]);
-                var player = GameManager.Instance.PrimaryPlayer;
-                var gun = player.inventory.CurrentGun;
-                var partner_gun = PickupObjectDatabase.GetById(partner_id) as Gun;
-                player.inventory.AddGunToInventory(partner_gun);
-                var forcer = gun.gameObject.AddComponent<DualWieldForcer>();
-                forcer.Gun = gun;
-                forcer.PartnerGunID = partner_gun.PickupObjectId;
-                forcer.TargetPlayer = player;
-                
+                if (_Chests.ContainsKey(args[0]))
+                {
+                    if (args.Length < 1) throw new Exception("At least 1 argument required.");
+                    var partner_id = int.Parse(args[0]);
+                    var player = GameManager.Instance.PrimaryPlayer;
+                    var gun = player.inventory.CurrentGun;
+                    var partner_gun = PickupObjectDatabase.GetById(partner_id) as Gun;
+                    player.inventory.AddGunToInventory(partner_gun);
+                    var forcer = gun.gameObject.AddComponent<DualWieldForcer>();
+                    forcer.Gun = gun;
+                    forcer.PartnerGunID = partner_gun.PickupObjectId;
+                    forcer.TargetPlayer = player;
+                }
+            });
+
+
+
+            _StatSet = (args) => {
+
+                float value;
+                if (_Stats.ContainsKey(args[0]) && float.TryParse(args[1], out value))
+                {
+                    GameManager.Instance.PrimaryPlayer.stats.SetBaseStatValue(_Stats[args[0]], value, GameManager.Instance.PrimaryPlayer);
+                    ETGModConsole.Log(_Stats[args[0]] + " set to: <color=#ff0000ff>" + value + "</color>");
+                }
+                else
+                    ETGModConsole.Log($"Please check your command and try again");
+            };
+
+            ETGModConsole.Commands.GetGroup("ms").AddUnit("spawnsynergy", (string[] args) =>
+            {
+                Chest synergy_Chest = GameManager.Instance.RewardManager.Synergy_Chest;
+                IntVector2 basePosition = new IntVector2((int)GameManager.Instance.PrimaryPlayer.transform.position.x, (int)GameManager.Instance.PrimaryPlayer.transform.position.y);
+                Chest.Spawn(synergy_Chest, basePosition);
             });
 
             _MsGroup = ETGModConsole.Commands.GetGroup("ms");
             _SetGroup = _MsGroup.AddUnit("set", _StatSet, _AutocompletionSettings);
             _SetGroup = _MsGroup.AddUnit("get", _StatGet, _AutocompletionSettings);
+            _SynergyChestGroup = _MsGroup.AddUnit("spawnglitch", _GlitchSpawn, _AutocompletionChests);
             _GetGroup = _MsGroup.AddUnit("getallstats", _StatGetAll, _AutocompletionSettings);
         }
 
